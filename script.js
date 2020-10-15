99128a9fed31067406b467201314029c3add8da6
kalinastoykova <kali0173@stud.kea.dk>
2020-10-15 09:17:32 +0200

    added footer, fixed a bit of css

--- script.js
+++ script.js
@@ -32,9 +32,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("nav_logo").src="res/logos/small_logo.png";
-      document.getElementById("first_a").style.color = "#000000";
-      document.getElementById("second_a").style.color = "#000000";
-      document.getElementById("third_a").style.color = "#000000";
+      document.getElementById("first_a").style.color = "#004155";
+      document.getElementById("second_a").style.color = "#004155";
+      document.getElementById("third_a").style.color = "#FFFFFF";
   } else {
     document.getElementById("nav_logo").src="res/logos/logo.png";
       document.getElementById("first_a").style.color = "#FFFFFF";
